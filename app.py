1e55b570bdfb9b11b23eabf3df92004115b68031
unknown <Felixlindblad.fl@gmail.com>
2020-03-26 11:09:26 +0100

    lite ändringar

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return render_template("index.html", movie=movie)
 
 if __name__ == "__main__":
-    app.run(host="movietime", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
