02206730d1a795a49dcba1f9199b62c512e73483
Adam Younes <54439799+AdamYounes@users.noreply.github.com>
2020-04-06 10:04:34 +0200

    Changed what "search :(" outputs
    Co-authored-by: Ludwig Sjöberg <ludwig.sjoberg@skola.botkyrka.se>
    Co-authored-by: Felix Lindblad <Felixlindblad.fl@gmail.com>

--- app.py
+++ app.py
@@ -5,10 +5,20 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         print(request.form.get("mycheckbox"))
-        return render_template('index.html', test='jäs')
+        return render_template('index.html', movies=movies)
 
     return render_template('index.html')
 
+movies = [
+    {
+        "name": "Interstellar",
+        "link": "https://www.imdb.com/title/tt0816692/?ref_=nv_sr_srsg_0"
+    },
+    {
+        "name": "Gladiator",
+        "link": "https://www.imdb.com/title/tt0172495/?ref_=fn_al_tt_1"
+    }
+]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
