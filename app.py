b7caa14bf54f34109b6a1b36100da8077b3385ed
Adam Younes <54439799+AdamYounes@users.noreply.github.com>
2020-04-09 11:51:18 +0200

    Enhanced what the checkboxes does

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request
+from json import load
 app = Flask(__name__)
+
+with open("movies.json", "r") as f:
+    movies = load(f)
+
+print(movies["action"][0]["title"])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form.get("mycheckbox"))
+        
+        print(request.form.get("action"))
+        print(request.form.get("comedy"))
+        print(request.form.get("drama"))
+        print(request.form.get("scifi"))
+        print(request.form.get("horror"))
+        print(request.form.get("thriller"))
+        print(request.form.get("romance"))
         return render_template('index.html', movies=movies)
 
     return render_template('index.html')
